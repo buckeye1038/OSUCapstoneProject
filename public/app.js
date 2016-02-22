34868d9d93fbc12092f0a0f83301a5a6280b5fcb
Umang Sharaf <umangs94@outlook.com>
2016-02-22 18:51:38 -0500

    UI changes

--- public/app.js
+++ public/app.js
@@ -2,8 +2,10 @@ var app = angular.module('meetingassistant', []);
 
 app.controller('MeetingAssistantController', function() {
     this.meetingsList = meetingMentions;
+    this.listening = false;
 });
 
+//JS Array containing the meetings
 var meetingMentions = [{
     date: new Date(2015, 1, 1),
     subject: "example subject",
@@ -14,9 +16,24 @@ var meetingMentions = [{
     subject: "example subject222",
     description: "example description222",
     enddate: new Date()
-},{
-    date: new Date(2017,8,25),
+}, {
+    date: new Date(2017, 8, 25),
     subject: "example subject future",
     description: "example description future",
     enddate: new Date()
 }];
+
+//function triggers when button is clicked
+function btnListen_Click() {
+    if (this.listening) {
+        $('#btnListen').text("Start listening");
+        $('#btnListen').addClass("btn-success");
+        $('#btnListen').removeClass("btn-warning");
+    } else {
+        $('#btnListen').text("Stop listening");
+        $('#btnListen').removeClass("btn-success");
+        $('#btnListen').addClass("btn-warning");
+    }
+
+    this.listening = !this.listening;
+};
