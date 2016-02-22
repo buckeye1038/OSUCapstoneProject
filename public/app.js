761b8135ea09c0cbeb0b598a4522a76a0c62f4d1
Umang Sharaf <umangs94@outlook.com>
2016-02-22 14:27:30 -0500

    Created controller and Google Calendar integration

--- public/app.js
+++ public/app.js
@@ -1,1 +1,22 @@
-var app = angular.module('store',[]);
+var app = angular.module('meetingassistant', []);
+
+app.controller('MeetingAssistantController', function() {
+    this.meetingsList = meetingMentions;
+});
+
+var meetingMentions = [{
+    date: new Date(2015, 1, 1),
+    subject: "example subject",
+    description: "example description",
+    enddate: new Date()
+}, {
+    date: new Date(),
+    subject: "example subject222",
+    description: "example description222",
+    enddate: new Date()
+},{
+    date: new Date(2017,8,25),
+    subject: "example subject future",
+    description: "example description future",
+    enddate: new Date()
+}];
